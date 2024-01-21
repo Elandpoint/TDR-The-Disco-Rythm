1f7c7daeba429e4ec67b068838e3cfc68423f94b
Elandpoint <123086932+Elandpoint@users.noreply.github.com>
2024-01-21 18:42:28 +0100

    MainMenu.cs

--- MainMenu.cs
+++ MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void MenuEspañol()
+    public void MenuCatalÃ ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
