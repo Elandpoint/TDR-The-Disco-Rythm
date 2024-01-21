854dfa19f6ec87c055c53a9b888abf426741191f
Elandpoint <123086932+Elandpoint@users.noreply.github.com>
2024-01-21 18:41:10 +0100

    Add files via upload

---
+++ SettingsMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class volumeslider : MonoBehaviour
+{
+    public Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (!PlayerPrefs.HasKey("Music"))
+        {
+            PlayerPrefs.SetFloat("Music", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("Music", volumeSlider.value);
+    }
+    public void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("Music");
+        AudioListener.volume = volumeSlider.value;
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
